728046c06319a243e746559029b60a971e40de83
mhlongophilani <mhlongophilani04@gmail.com>
2024-05-09 10:50:07 +0200

    Added SEO-friendly URL for post

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,get_object_or_404
 from django.http import Http404
+from django.views.generic import ListView
 # Create your views here.
 
 from .models import Post
@@ -10,10 +11,23 @@ def post_list(request):
 
 
 # a view to display a single post
-def post_detail(request,id):
+def post_detail(request,year,month,day,post):
     
     # We use this to retrive the desired post
-    post=get_object_or_404(Post,id=id,status=Post.Status.PUBLISHED)
-    
+    post = get_object_or_404(Post,
+                             status=Post.Status.PUBLISHED,
+                             slug=post,
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
     
     return render(request,'blog/post/details.html',{'post':post})
+
+class PostListView(ListView):
+    """
+    Alternative post list view
+    """
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
