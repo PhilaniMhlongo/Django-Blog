1d7c0ce51c24f470d3488d6c3128af577fc04125
mhlongophilani <mhlongophilani04@gmail.com>
2024-06-11 04:42:45 +0200

    Added Email Share System

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -124,3 +124,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Email server configuration
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'mhlongophilani04@gmail.com'
+EMAIL_HOST_PASSWORD = 'kate mecx ohkg lwgh'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
